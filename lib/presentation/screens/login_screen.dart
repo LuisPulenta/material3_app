74c97a54d5fcf1e2146dce3d14a602296b5e5af5
LuisPulenta <luisalbertonu@gmail.com>
2023-06-15 21:33:56 -0300

    Login

--- lib/presentation/screens/login_screen.dart
+++ lib/presentation/screens/login_screen.dart
@@ -75,87 +75,103 @@ class _LoginFormState extends State<_LoginForm> {
     final colors = Theme.of(context).colorScheme;
     final loginCubit = context.watch<LoginCubit>();
 
-    return Card(
-      shape: RoundedRectangleBorder(
-        side: BorderSide(
-          color: colors.primary,
-        ),
-        borderRadius: BorderRadius.circular(10.0),
+    return Container(
+      decoration: BoxDecoration(
+        boxShadow: [
+          BoxShadow(
+            color: colors.primary.withOpacity(.5),
+            blurRadius: 20.0,
+            spreadRadius: 0.0,
+            offset: const Offset(
+              10.0,
+              10.0,
+            ),
+          ),
+        ],
       ),
-      //elevation: 3,
-      color: colors.secondaryContainer,
-      child: Form(
-        key: _formKey,
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 20),
-          child: Column(
-            children: [
-              const SizedBox(
-                height: 20,
-              ),
-              CustomTextFormField(
-                label: 'Correo electrónico',
-                hint: 'Ingrese Correo electrónico...',
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Campo requerido';
-                  }
-                  if (value.trim().isEmpty) return 'Campo requerido';
-                  final emailRegExp = RegExp(
-                    r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$',
-                  );
-                  if (!emailRegExp.hasMatch(value)) {
-                    return 'No tiene formato de correo';
-                  }
-                  return null;
-                },
-                errorMessage: null,
-                prefixIcon: Icons.email,
-                onChanged: (value) {
-                  loginCubit.emailChanged(value);
-                  _formKey.currentState!.validate();
-                },
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              CustomTextFormField(
-                label: 'Contraseña',
-                hint: 'Ingrese Contraseña...',
-                validator: (value) {
-                  if (value == null || value.isEmpty) return 'Campo requerido';
-                  if (value.trim().isEmpty) return 'Campo requerido';
-                  if (value.trim().length < 6) return 'Más de 6 letras';
-                  return null;
-                },
-                errorMessage: null,
-                prefixIcon: Icons.password,
-                suffixIcon: Icons.remove_red_eye,
-                onChanged: (value) {
-                  loginCubit.passwordChanged(value);
-                  _formKey.currentState!.validate();
-                },
-                obscureText: true,
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              CustomButton(
-                color: colors.inversePrimary,
-                text: 'Ingresar',
-                icon: Icons.login,
-                width: double.infinity,
-                height: 54,
-                onPressed: () {
-                  bool isvalid = _formKey.currentState!.validate();
-                  if (!isvalid) return;
-                  loginCubit.onSubmit();
-                },
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-            ],
+      child: Card(
+        shape: RoundedRectangleBorder(
+          side: BorderSide(
+            color: colors.primary,
+          ),
+          borderRadius: BorderRadius.circular(10.0),
+        ),
+        elevation: 30,
+        color: colors.secondaryContainer,
+        child: Form(
+          key: _formKey,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: Column(
+              children: [
+                const SizedBox(
+                  height: 20,
+                ),
+                CustomTextFormField(
+                  label: 'Correo electrónico',
+                  hint: 'Ingrese Correo electrónico...',
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Campo requerido';
+                    }
+                    if (value.trim().isEmpty) return 'Campo requerido';
+                    final emailRegExp = RegExp(
+                      r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$',
+                    );
+                    if (!emailRegExp.hasMatch(value)) {
+                      return 'No tiene formato de correo';
+                    }
+                    return null;
+                  },
+                  errorMessage: null,
+                  prefixIcon: Icons.email,
+                  onChanged: (value) {
+                    loginCubit.emailChanged(value);
+                    _formKey.currentState!.validate();
+                  },
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                CustomTextFormField(
+                  label: 'Contraseña',
+                  hint: 'Ingrese Contraseña...',
+                  validator: (value) {
+                    if (value == null || value.isEmpty)
+                      return 'Campo requerido';
+                    if (value.trim().isEmpty) return 'Campo requerido';
+                    if (value.trim().length < 6) return 'Más de 6 letras';
+                    return null;
+                  },
+                  errorMessage: null,
+                  prefixIcon: Icons.password,
+                  suffixIcon: Icons.remove_red_eye,
+                  onChanged: (value) {
+                    loginCubit.passwordChanged(value);
+                    _formKey.currentState!.validate();
+                  },
+                  obscureText: true,
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                CustomButton(
+                  color: colors.inversePrimary,
+                  text: 'Ingresar',
+                  icon: Icons.login,
+                  width: double.infinity,
+                  height: 54,
+                  onPressed: () {
+                    bool isvalid = _formKey.currentState!.validate();
+                    if (!isvalid) return;
+                    loginCubit.onSubmit();
+                  },
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+              ],
+            ),
           ),
         ),
       ),
